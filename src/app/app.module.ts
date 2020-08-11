9e0666fa19ad7be57b49a505e065c83758368206
alonsox <alonsox@users.noreply.github.com>
2020-08-11 11:32:49 -0400

    Created weather service template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule],
+  imports: [BrowserModule, CoreModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WeatherService } from './weather.service';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  providers: [WeatherService],
+})
+export class CoreModule {}
