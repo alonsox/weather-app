d59e15420884946b2b6774bf6433413197dcb4fe
alonsox <alonsox@users.noreply.github.com>
2020-08-11 17:28:53 -0400

    Changed code formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
@@ -10,8 +11,13 @@ import { WheaterInfoComponent } from './wheater-info/wheater-info.component';
 import { SettingsFormComponent } from './settings-form/settings-form.component';
 
 @NgModule({
-  declarations: [AppComponent, SearchbarComponent, WheaterInfoComponent, SettingsFormComponent],
-  imports: [BrowserModule, CoreModule, SharedModule],
+  declarations: [
+    AppComponent,
+    SearchbarComponent,
+    WheaterInfoComponent,
+    SettingsFormComponent,
+  ],
+  imports: [BrowserModule, ReactiveFormsModule, CoreModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
