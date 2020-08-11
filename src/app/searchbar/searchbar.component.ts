acde02bcbf733dd5c4a055ac41c27d632d8f4bde
alonsox <alonsox@users.noreply.github.com>
2020-08-11 18:19:38 -0400

    Implemented searchbar and its validation

--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -1,12 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { WeatherService } from '../core/weather.service';
 
 @Component({
   selector: 'app-searchbar',
   templateUrl: './searchbar.component.html',
-  styleUrls: ['./searchbar.component.html'],
+  styleUrls: ['./searchbar.component.css'],
 })
 export class SearchbarComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  errorMessage = '';
+  cityInfo = new FormGroup({
+    cityName: new FormControl('', [Validators.required]),
+  });
+
+  constructor(private weatherService: WeatherService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit(): void {
+    if (this.cityInfo.valid) {
+      this.errorMessage = '';
+      this.weatherService.searchCity(this.cityInfo.value.cityName);
+    } else {
+      this.errorMessage = 'Enter the name of a city';
+    }
+  }
 }
